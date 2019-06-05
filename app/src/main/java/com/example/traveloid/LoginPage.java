5456b0ca22608b4737a9be7b5956aeb8b25c1fbf
souvik <41781438+Souvikns@users.noreply.github.com>
2019-06-05 21:35:26 +0530

    Signup connect kardia

--- app/src/main/java/com/example/traveloid/LoginPage.java
+++ app/src/main/java/com/example/traveloid/LoginPage.java
@@ -1,7 +1,12 @@
 package com.example.traveloid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.BufferedReader;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -9,5 +14,10 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+    }
+    public void signup(View view){
+        Intent intent=new Intent(this,singnup.class);
+        startActivity(intent);
     }
 }
