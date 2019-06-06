91c922a8869fd6e0393e781e6ec061a3e70bf31a
sy0837 <50517233+sy0837@users.noreply.github.com>
2019-06-06 13:19:12 +0530

    homepage renew

--- app/src/main/java/com/example/traveloid/homepage.java
+++ app/src/main/java/com/example/traveloid/homepage.java
@@ -10,7 +10,7 @@ public class homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
-        tab
+
 
     }
 }
