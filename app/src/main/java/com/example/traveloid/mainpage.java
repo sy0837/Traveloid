e0157d92279bff077dac64e22ad67730e2a12a01
souvik <41781438+Souvikns@users.noreply.github.com>
2019-06-07 12:54:34 +0530

    Settings button connected

--- app/src/main/java/com/example/traveloid/mainpage.java
+++ app/src/main/java/com/example/traveloid/mainpage.java
@@ -9,5 +9,6 @@ public class mainpage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainpage);
+        //example
     }
 }
