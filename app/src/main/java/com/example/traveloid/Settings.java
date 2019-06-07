82d03cde36bad89e473f06ca9700217df63f0f77
souvik <41781438+Souvikns@users.noreply.github.com>
2019-06-07 13:21:33 +0530

    Settings button connected

--- app/src/main/java/com/example/traveloid/Settings.java
+++ app/src/main/java/com/example/traveloid/Settings.java
@@ -1,7 +1,9 @@
 package com.example.traveloid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Settings extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+    public void donebtn(View view){
+        Intent intent =new Intent(this,homepage.class);
+        startActivity(intent);
     }
 }
