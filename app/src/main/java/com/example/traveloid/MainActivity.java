20f948ea563908fc1657f5942d9195708946c230
souvik <41781438+Souvikns@users.noreply.github.com>
2019-06-07 10:24:30 +0530

    spash creen added

--- app/src/main/java/com/example/traveloid/MainActivity.java
+++ app/src/main/java/com/example/traveloid/MainActivity.java
@@ -6,29 +6,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    int n=2;
-    boolean stopTimer=false;
+    public static int SPLASH_TIME_OUT=1000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Handler handler= new Handler();
-        handler.post(new Runnable() {
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                n--;
-                if(n<0){
-                    stopTimer=true;
-                }
-                if(stopTimer==false){
-                    handler.postDelayed(this,1000);
-                }
-                if(stopTimer==true){
-                    Intent intent=new Intent(MainActivity.this,LoginPage.class);
-                    startActivity(intent);
-                }
+                Intent homestart=new Intent(MainActivity.this,MainActivity.class);
+                startActivity(homestart);
+                finish();
+
             }
-        });
+        },SPLASH_TIME_OUT);
     }
 }
