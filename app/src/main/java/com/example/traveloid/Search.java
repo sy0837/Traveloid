134cfbe8af0e0f071643d529268688cc97795060
sy0837 <50517233+sy0837@users.noreply.github.com>
2019-06-08 15:21:28 +0530

    many changes done

--- app/src/main/java/com/example/traveloid/Search.java
+++ app/src/main/java/com/example/traveloid/Search.java
@@ -2,12 +2,19 @@ package com.example.traveloid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 public class Search extends AppCompatActivity {
+    int images[]={R.drawable.dubai,R.drawable.mountain,R.drawable.london,R.drawable.bangkok};
+    GridView grid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        grid=findViewById(R.id.grid1);
+
+
     }
 }
--- app/src/main/java/com/example/traveloid/user.java
+++ app/src/main/java/com/example/traveloid/user.java
@@ -6,12 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class user extends Fragment {
+
 
 
     public user() {
@@ -22,8 +24,11 @@ public class user extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_user, container, false);
+
+
     }
 
 }
