4ce9881c0405bc172c8c6628053e236dd51db76a
sy0837 <50517233+sy0837@users.noreply.github.com>
2019-06-08 12:51:14 +0530

    user done

--- app/src/main/java/com/example/traveloid/travel.java
+++ app/src/main/java/com/example/traveloid/travel.java
@@ -2,7 +2,9 @@ package com.example.traveloid;
 
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,9 @@ import android.view.ViewGroup;
  * A simple {@link Fragment} subclass.
  */
 public class travel extends Fragment {
+    int tabicons1[]={R.drawable.flight,R.drawable.train,R.drawable.bus,R.drawable.taxi};
+    TabLayout t1;
+    ViewPager v2;
 
 
     public travel() {
@@ -19,11 +24,14 @@ public class travel extends Fragment {
     }
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_travel, container, false);
+
+
     }
 
 }
